38476057742dcc532a02d861948c7214937ad098
Eric Gulotty <ericgulottyjr@gmail.com>
2024-03-20 20:07:54 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# stop_ids: 70106 70107 70237 70238 70160 70161 70503 70504 70511 70512 70174 70175
+
+stop_dict = {
+    'Boston College': [70106, 70107],
+    'Cleveland Circle': [70237, 70238],
+    'Riverside': [70160, 70161],
+    'Union Square': [70503, 70504],
+    'Medford Tufts': [70511, 70512],
+    'Reservoir': [70174, 70175]
+}
